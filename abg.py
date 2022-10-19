ee5661b57649f866ec6d58e9aafbdd8c6499bec8
Nikita <nikita.volenberg@gmail.com>
2022-10-19 12:42:29 +0600

    Занесли поиск минимального элемента в функцию

--- abg.py
+++ abg.py
@@ -2,11 +2,12 @@ massiv=[]
 for i in range(5):
     massiv.append(int(input()))
 
+def minfunc():
+    min_=massiv[0]
+    for i in range(5):
+        if(massiv[i]<min_):
+            min_=massiv[i]
+    return min_
 
-min_=massiv[0]
-for i in range(5):
-    if(massiv[i]<min_):
-        min_=massiv[i]
 
-
-print(min_)
+print(minfunc())
